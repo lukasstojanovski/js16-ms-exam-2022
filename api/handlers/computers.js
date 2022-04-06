8470b9ab1ed0f4f76db37767abb96a5d2f550dc2
lukasstojanovski <lukas.stojanovski@gmail.com>
2022-04-06 21:36:41 +0200

    5

--- api/handlers/computers.js
+++ api/handlers/computers.js
@@ -2,9 +2,10 @@ const computers = require("../pkg/computers")
 
 const getAll = async (req, res) => {
     try{
-        
+        let pcs = await computers.getAll();
+        return res.status(200).send(pcs)
     }catch(err) {
         console.log(err);
         return res.status(500).send("server error")
     }
-}
+};
